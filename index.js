277f328dc357b460b8d00eb7b683e112ba9ac31e
Felj Abas <felabas41@gmail.com>
2020-08-24 23:11:04 -0700

    updated

---
+++ index.js
@@ -0,0 +1,52 @@
+const STORE = [
+    {
+        question:`Prior to joining the X-Men, where was Cyclops raised?`,
+        answers:[
+            'At the zoo',
+            'on the far side of the moon',
+            'in an orphanage',
+            'Cincinatti'
+        ],
+        correctAnswer:'in an orphanage',
+    },
+    {
+        question:`After going through the Seige Perilous, British X-man Psylocke's consciousness was split in half with an assassin from this country.`,
+        answers:[
+            'USA',
+            'Canada',
+            'Israel',
+            'Japan'
+        ],
+        correctAnswer:'Japan',
+    },
+    {
+        question:`During her time with the Brotherhood of Mutants, Rogue faced off with this hero, stealing her powers of flight and invulnerability in the process `,
+        answers:[
+            'Ms Marvel AKA Carol Danvers',
+            'White Queen AKA Emma Frost',
+            'Husk AKA Paige Guthrie',
+            'Dazzler AKA Alison Blaire'
+        ],
+        correctAnswer:'Ms Marvel AKA Carol Danvers',
+    },
+    {
+        question:`Erik Magnus Lensherr is infamous across the world, is more commonly known by this name`,
+        answers:[
+            'Profeesor X',
+            'The Blob',
+            'Proteus',
+            'Magneto'
+        ],
+        correctAnswer:'Magneto',
+    },
+    {
+        question:`This white haired mutant powerhouse was raised as a pickpocket in Cario before eventually becoming leader of the X-men`,
+        answers:[
+            'Storm',
+            'Bishop',
+            'Iceman',
+            'Jean Grey'
+        ],
+        correctAnswer:'Jean Grey',
+    }, 
+ ];
