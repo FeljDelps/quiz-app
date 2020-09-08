08dfda4a2f5afa39b210a389990e37d9e19a57e8
Felj Abas <felabas41@gmail.com>
2020-09-07 20:16:18 -0700

    updated 9/7/20 and compressed photos

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const STORE = [
             'Iceman',
             'Jean Grey'
         ],
-        correctAnswer:'Jean Grey',
+        correctAnswer:'Storm',
     }, 
  ];
 
@@ -62,53 +62,12 @@ function generateQuestion(){
       //function should take objects & create HTML string that will be displayed in the DOM
       return createThing(questionNumber); 
     } else{
-      //insert final instructions 
+      //insert final instructions
+      $('.questionBox').hide();
+      finalScore();
     }
      console.log('`generateQuestion` ran');
 }
-
-function createThing(questionIndex){
-   //this function creates the HTML string for objects in STORE that will be used in generateQuestion
-   
-   //the for element, as well as the question, which is setup in a legend element in its own fieldset is created first
-   let formMaker = $(`<form>
-    <fieldset>
-      <legend class='questionText'>${STORE[questionIndex].question}</legend>
-    </fieldset>
-</form>`)
-
-//a variable for the fieldset is created, so that we can easily append the answers to it.
-  let fieldSelector = $(formMaker).find('fieldset');
-
-//here we'll create the html for the answers, and the append it to fieldselector(fieldset element)
- STORE[questionIndex].answers.forEach(function(answerValue, answerIndex){
-  $(`<label class='sizeMe' for='${answerIndex}'>
-  <input class='radio' type='radio' id='${answerIndex}' value='${answerValue}' name='answer' required>
-  <span>${answerValue}</span>
-</label>`).appendTo(fieldSelector);
- });
-
- //here we'll create the html for the answer button, and append it to fieldselector(fieldset element)
- $(`<button type='submit' class='submitButton button'>Submit</button>`).appendTo(fieldSelector);
-
-//we now return the completed html form element
- return formMaker;
-}
-
-
-/*
-FORM HTML
-<form>
-    <fieldset>
-      <legend class='questionText'>${STORE[questionIndex].question}</legend>
-    </fieldset>
-<label class='sizeMe' for='${answerIndex}'>
-  <input class='radio' type='radio' id='${answerIndex}' value='${answerValue}' name='answer' required>
-  <span>${answerValue}</span>
-</label>
-<button type='submit' class='submitButton button'>Submit</button>
-</form>
-*/
 
 //this function increments the score variable by 1, then updates the value in the text in the quiz
 function updateScore(){
@@ -119,10 +78,16 @@ function updateScore(){
 //this function increments the questionNumber variable by 1, then updates the question number text in the quiz.  Since the questionNumber variable indicates array index and starts at 0, we add +1 to the visible text in the quiz.
 function updateQuestionNumber(){
   questionNumber++;
-  $('.questionNumber').text(questionNumber+1)
+  $('.questionNumber').text(questionNumber + 1);
 }
 
-
+//this function ressets the score and questionNumber variables
+function resetStats(){
+  let score=0;
+  let questionNumber=0;
+  $('.score').text('0');
+  $('.questionNumber').text('0');
+}
 
 function startQuiz(){
      //responsible for starting the quiz app
@@ -143,22 +108,6 @@ function startQuiz(){
 }
 
 
-function correctAnswer(){
-  //this function generates when answer is correct, and renders the html for correct answers, which include a heading, picture, and button that leads the user to the next question when clicked, it then updates the score variable as well as the score shown on the quiz
-  $('.answer').html(`
-    <h3>Correct!</h3>
-    <button type='button' class='nextButton button'>Next</button>
-  `);
-  updateScore();
-}
-
-function wrongAnswer(){
-  $('.answer').html(`
-    <h3>Wrong!</h3>
-    <h2>Correct Answer:  <span>${STORE[questionNumber].correctAnswer}</span>
-    <button type='button' 'class'nextButton button'>Next</button>
-  `);
-}
 
 function submitAnswer(){
      //responsible for submitting answered questions from the quiz app
@@ -184,23 +133,143 @@ function submitAnswer(){
   });        
  }
 
- function nextQuestion(){
+ function createThing(questionIndex){
+  //this function creates the HTML string for objects in STORE that will be used in generateQuestion
+  
+  //the for element, as well as the question, which is setup in a legend element in its own fieldset is created first
+  let formMaker = $(`<form>
+   <fieldset>
+     <legend class='questionText'>${STORE[questionIndex].question}</legend>
+   </fieldset>
+</form>`)
+
+//a variable for the fieldset is created, so that we can easily append the answers to it.
+ let fieldSelector = $(formMaker).find('fieldset');
+
+//here we'll create the html for the answers, and the append it to fieldselector(fieldset element)
+STORE[questionIndex].answers.forEach(function(answerValue, answerIndex){
+ $(`<label class='sizeMe' for='${answerIndex}'>
+ <input class='radio' type='radio' id='${answerIndex}' value='${answerValue}' name='answer' required>
+ <span>${answerValue}</span>
+</label>`).appendTo(fieldSelector);
+});
+
+//here we'll create the html for the answer button, and append it to fieldselector(fieldset element)
+$(`<button type='submit' class='submitButton button'>Submit</button>`).appendTo(fieldSelector);
+
+//we now return the completed html form element
+return formMaker;
+}
+
+
+/*
+FORM HTML
+<form>
+   <fieldset>
+     <legend class='questionText'>${STORE[questionIndex].question}</legend>
+   </fieldset>
+<label class='sizeMe' for='${answerIndex}'>
+ <input class='radio' type='radio' id='${answerIndex}' value='${answerValue}' name='answer' required>
+ <span>${answerValue}</span>
+</label>
+<button type='submit' class='submitButton button'>Submit</button>
+</form>
+*/
+
+function correctAnswer(){
+  //this function generates when answer is correct, and renders the html for correct answers, which include a heading, picture, and button that leads the user to the next question when clicked, it then updates the score variable as well as the score shown on the quiz
+  $('.answer').html(`
+    <h3>Correct!</h3>
+    <img src='compressed photos/storm.jpg' alt='picture of Storm' class='images' width='200px'>
+    <button type='button' class='nextButton button'>Next</button>`
+  );
+  updateScore();
+}
+
+function wrongAnswer(){
+  $('.answer').html(`
+    <h3>Wrong!</h3>
+    <h2>Correct Answer:  <span>${STORE[questionNumber].correctAnswer}</span>
+    <img src='compressed photos/magneto.jpg' alt='picture of Magneto' class='images' width='200px'>
+    <button type='button' class='nextButton button'>Next</button>`
+  );
+}
+
+
+function nextQuestion(){
      //responsible for moving to next question on the quiz app
      //listen for when user clisks on 'next question;
-     //hide alt boxes
-     //show question box
+  $('.displayBox').on('click', '.nextButton', function(event){
+      //hide alt boxes
+    $('.altBox').hide();
+    //show question box
+    $('.questionBox').show();
      //update question number
-     //update with new question
+     updateQuestionNumber();
+     //update questionBox with new question
+     $('.questionBox form').replaceWith(generateQuestion);
      console.log('`nextQuestion` ran');
+  });
+}
+
+function finalScore(){
+//this function determines the final score, and updates the html 
+$('.final').show();
+
+//create arrays of different messages players receive depending on their score
+let great =[
+  'Awesome job! Welcome to the X-Men!',
+  'compressed photos/cyclops.jpg',
+  'picture of Cyclops shirtless'
+];
+
+let good = [
+  'Not bad! Keep up the good work',
+  'compressed photos/shadowcat.png',
+  'picture of Shadowcat'
+];
+
+let bad = [
+  'Mmm.. not so good.  Try again',
+  'compressed photos/rogue v ms marvel.jpg',
+  'picture of Rogue fighting Ms Marvel'
+];
+
+//create a function that displays the correct array based off their score.  Insert pictures on each array, then modify this code.
+
+if(score >= 5){
+  array=great;
+} else if(score < 3 && score > 4){
+  array = good;
+} else{
+  array=bad;
+}
+
+// display the final array in 'final' html element
+
+return $('.final').html(
+`<h3>${array[0]}</h3>
+<img src='${array[1]}' alt='${array[2]}' class='images'>
+<h2>Your final score it ${score}/10</h2>
+<button type='submit' class='restartButton button'>Restart quiz</button>`
+);
 }
 
 function restartQuiz(){
      //responsible for restarting the quiz app once all questions are answered
      //listen for when user clicks on restart button
-     //reset question number and score
+$('.displayBox').on('click', '.restartButton', function(event){
+    event.preventDefault();  
+  //reset question number and score
+    resetStats();
      //hide alt boxes
+     $('.altBox').hide();
      //show startQuiz box
+     $('.startQuiz').show();
      console.log('`restartQuiz` ran');
+     
+
+});  
 }
 
  //the callback function responsible for running all functions 
